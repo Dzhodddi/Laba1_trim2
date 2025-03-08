637323a80a97ae926dded9bf3788bc90bf5960e4
Dzzhodddi <d.dzhos@ukma.edu.ua>
2025-03-08 12:01:59 +0200

    Add searching methods for Cathedra class

--- Laba_1/src/main/java/org/example/Main.java
+++ Laba_1/src/main/java/org/example/Main.java
@@ -15,24 +15,31 @@ class Human {
 	}
 	
 	public String toString() {
-		return "My name is: " + name + " " + surname + " " + middlename + " ,and I'm working as a " + employment;
+		return "My name is: " + name + " " + surname + " " + middlename + " I'm " + age + " ,and I'm working as a " + employment;
 	}
 	public String getName() {
 		return name + " " + surname;
 	}
+
+	public String getFullName() {
+		return getName() + " " + middlename;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public boolean toEquals(Human human) {
+		return human.getFullName().equals(getFullName());
+	}
 }
 
 class Professor extends Human{
-    private String name;
-    private String surname;
-    private String middlename;
-    private int age;
 	private String cathedra;
 	private String faculty;
-	private int salary;
 	private  String [] groups;
 	private  String [] students;
-	Professor(String name, String surname, String middlename, int age, String cathedra, String Faculty) {
+	Professor(String name, String surname, String middlename, int age, String cathedra, String faculty) {
 		super(name, surname, middlename, age, "Professor");
 		this.cathedra = cathedra;
 		this.faculty = faculty;
@@ -43,24 +50,169 @@ class Professor extends Human{
 		return super.toString() + " in university at " + cathedra + " cathedra in " + faculty + " faculty"; 
 	}
 
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public String getCathedra() {
+		return cathedra;
+	}
+
+	private void addStudents(Student [] students) {
+
+	}
+
+}
+
+class Student extends Human {
+	private String name;
+	// TO-DO variables
+	private int year;
+	private int group;
+	Student(String name, String surname, String middlename, int age, int year) {
+		super(name, surname, middlename, age, "Student");
+		this.year = year;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public int getGroup() {
+		return group;
+	}
+
 }
 
 class Cathedra {
 	private String name;
 	private Professor [] professors;
-//	private Students [] students;
-	
+	private Student [] students;
+	Cathedra(String name) {
+		this.name = name;
+	}
+
+	public void orderedStudentsAlphabetically() {
+		Student [] orderedStudents = (Student[]) Utils.quickSortByName(students, 0, students.length - 1);
+		System.out.print("Ordered students alphabetically: ");
+		for (Student student : students) {
+			System.out.println(student);
+		}
+	}
+
+	public void orderedProfessorsAlphabetically() {
+		Professor [] orderedProfessors = (Professor[]) Utils.quickSortByName(professors, 0, professors.length - 1);
+		System.out.print("Ordered professors alphabetically: ");
+		for (Professor professor : professors) {
+			System.out.println(professor);
+		}
+	}
+
+	public Student [] selectStudentsByYear(int year) {
+
+		int counter = 0;
+//		System.out.print("Students that student in year " + year + " : ");
+		for (Student student : students) {
+			if (student.getYear() == year) {
+				System.out.println(student);
+				counter++;
+			}
+		}
+		Student [] selectStudentsByYear = new Student[counter];
+		counter = 0;
+        for (Student student : students) {
+            if (student.getYear() == year) {
+                selectStudentsByYear[counter] = student;
+                counter++;
+            }
+        }
+		return selectStudentsByYear;
+	}
+
+	public void orderedStudentsAlphabeticallyInSelectedYear(int year) {
+		Student [] orderedStudents = (Student[]) Utils.quickSortByName(selectStudentsByYear(year), 0, students.length - 1);
+		System.out.print("Ordered students by name with year " + year + " alphabetically: ");
+		for (Student student : students) {
+			System.out.println(student);
+		}
+	}
+
+	public int amountOfStudents() {
+		return students.length;
+	}
 }
 
-class Faculty {
 
-}
+//class Faculty {
+//	private String name;
+//	private Cathedra [] cathedrae;
+//	Faculty(String name) {
+//		this.name = name;
+//	}
+//
+//	public Cathedra [] getCathedrae() {
+//		return cathedrae;
+//	}
+//
+//	public int amountOfStudents() {
+//		int counter = 0;
+//		for (Cathedra cathedra : cathedrae) {
+//			counter += cathedra.amountOfStudents();
+//		}
+//		return counter;
+//	}
+//}
 
-class Student {
-
-}
-
-class University {
-
-}
+//class University {
+//	private String name;
+//	private Faculty [] faculties;
+//	private int amountOfStudents = 0;
+//	private Student [] students;
+//	private Professor [] professors;
+//	University(String name) {
+//		this.name = name;
+//	}
+//
+//	public void findStudent(String name) {
+//		for (Student student : students) {
+//			if (student.getName().equals(name)) {
+//				System.out.println(student);
+//			}
+//		}
+//	}
+//
+//	public void findStudents(String name, int year) {
+//		for (Student student : students) {
+//			if (student.getName().equals(name) && student.getYear() == year) {
+//				System.out.println(student);
+//			}
+//		}
+//	}
+//
+//	public void findStudents(String name, int year, int group) {
+//		for (Student student : students) {
+//			if (student.getName().equals(name) && student.getYear() == year && student.getGroup() == group) {
+//				System.out.println(student);
+//			}
+//		}
+//	}
+//
+//	public void findProfessor(String name) {
+//		for (Professor professor : professors) {
+//			if (professor.getName().equals(name)) {
+//				System.out.println(professor);
+//			}
+//		}
+//	}
+//
+//	public void findProfessor(String name, int [] group) {
+//		for (Professor professor : professors) {
+//			if (professor.getName().equals(name)) {
+//				System.out.println(professor);
+//			}
+//		}
+//	}
+//
+//
+//}
 
--- Laba_1/src/main/java/org/example/Utils.java
+++ Laba_1/src/main/java/org/example/Utils.java
@@ -24,14 +24,14 @@ public class Utils {
         return 1;
     }
 
-	   public static Human[] quickSortByName(Human[] a, int beginIndex, int endIndex ){
+    public static Human[] quickSortByName(Human[] a, int beginIndex, int endIndex ){
         if (beginIndex < endIndex) {
             int dividingIndex = getDividingIndexString(a, beginIndex, endIndex);
             quickSortByName(a, beginIndex, dividingIndex - 1);
             quickSortByName(a, dividingIndex + 1, endIndex);
         }
         return a;
-    }
+     }
 
 	private static int getDividingIndexString(Human [] a, int beginIndex, int endIndex) {
         String key = a[endIndex].getName();
@@ -54,4 +54,31 @@ public class Utils {
 
 	}
 
+    private static void swapStudents(Student [] a, int i, int j) {
+         Student temp = a[i];
+         a[i] = a[j];
+         a[j] = temp;
+    }
+
+    private static int getDividingIndexInt(Student [] a, int beginIndex, int endIndex) {
+        int key = a[endIndex].getYear();
+        int i = beginIndex - 1;
+        for (int j = beginIndex; j < endIndex; j++) {
+            if (a[j].getYear() < key) {
+                i++;
+                swapStudents(a, i, j);
+            }
+        }
+        swapStudents(a, i + 1, endIndex);
+        return i + 1;
+    }
+
+    public static Student[] quickSortByYear(Student[] a, int beginIndex, int endIndex ){
+        if (beginIndex < endIndex) {
+            int dividingIndex = getDividingIndexInt(a, beginIndex, endIndex);
+            quickSortByYear(a, beginIndex, dividingIndex - 1);
+            quickSortByYear(a, dividingIndex + 1, endIndex);
+        }
+        return a;
+    }
 }
