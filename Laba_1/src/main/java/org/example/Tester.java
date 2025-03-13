bae8e279c51220fbf535fc972a3a0679dc18b3c2
Гандзюр Дмитро <handziurdmytro@gmail.com>
2025-03-13 14:11:28 +0200

    tester

--- Laba_1/src/main/java/org/example/Tester.java
+++ Laba_1/src/main/java/org/example/Tester.java
@@ -1,58 +1,6 @@
 package org.example;
 
 import java.io.IOException;
-
-public class Tester {
-    public static void main(String[] args) throws IOException {
-        Cathedra cathedra = createCathedra("FI");
-
-    }
-
-
-    private static Human createHuman() throws IOException {
-        String name = DataInput.getString("Input name: ");
-        String surname = DataInput.getString("Input surname: ");
-        String middlename = DataInput.getString("Input middle name: ");
-        int age = DataInput.getNotNegativeInt("Input age: ");
-//        String employment = DataInput.getString("Input employment: ");
-        return new Human(name, surname, middlename, age, "");
-    }
-
-    private static Student createStudent(String cathedra, String faculty) throws IOException {
-        Human human = createHuman();
-        int year = DataInput.validateNumber("Input year of student: ", 1, 4);
-        int group = DataInput.validateNumber("Input group of student: ", 1, 10);
-        return new Student(human.getFirstName(), human.getSurname() , human.getMiddlename(), human.getAge() ,year, group, cathedra, faculty);
-    }
-
-
-    private static Professor createProfessor(String cathedra, String faculty) throws IOException {
-        Human human = createHuman();
-        return new Professor(human.getFirstName(), human.getSurname() , human.getMiddlename(), human.getAge() , cathedra, faculty);
-    }
-
-    private static Cathedra createCathedra(String faculty) throws IOException {
-        String name = DataInput.getString("Input cathedra' name: ");
-        int amountOfStudents = DataInput.validateNumber("Input amount of students: ", 1, 100);
-        int amountOfProfessors = DataInput.validateNumber("Input amount of professors: ", 1, 100);
-        Student [] students = new Student[amountOfStudents];
-        Professor [] professors = new Professor[amountOfProfessors];
-        System.out.println("Set students: ");
-        for (int i = 0; i < amountOfStudents; i++) {
-            Student student = createStudent(name, faculty);
-            students[i] = student;
-            System.out.println("Student created");
-        }
-        System.out.println("Set professors: ");
-        for (int i = 0; i < amountOfProfessors; i++) {
-            Professor professor = createProfessor(name, faculty);
-            professors[i] = professor;
-            System.out.println("Professor created");
-        }
-        return new Cathedra(name, students, professors);
-    }
-}
-import java.io.IOException;
 // test giy
 public class Tester {
     public static void main(String[] args) throws IOException {
@@ -72,7 +20,7 @@ public class Tester {
         }
 
         String universityName = DataInput.getString("Write down a University name: ");
-        University university = new University(universityName);
+        //University university = new University(universityName);
         System.out.println("\n\t** created new University called \""+universityName+"\" **");
 
         while(true){
@@ -252,6 +200,49 @@ public class Tester {
                 "\n\t8. Вивести всіх студентів/викладачів кафедри впорядкованих за алфавітом" +
                 "\n\t9. Вивести всіх студентів кафедри вказаного курсу " +
                 "\n\t10. Вивести всіх студентів кафедри вказаного курсу впорядкованих за алфавітом");
+    }
+
+    private static Human createHuman() throws IOException {
+        String name = DataInput.getString("Input name: ");
+        String surname = DataInput.getString("Input surname: ");
+        String middlename = DataInput.getString("Input middle name: ");
+        int age = DataInput.getNotNegativeInt("Input age: ");
+//        String employment = DataInput.getString("Input employment: ");
+        return new Human(name, surname, middlename, age, "");
+    }
+
+    private static Student createStudent(String cathedra, String faculty) throws IOException {
+        Human human = createHuman();
+        int year = DataInput.validateNumber("Input year of student: ", 1, 4);
+        int group = DataInput.validateNumber("Input group of student: ", 1, 10);
+        return new Student(human.getFirstName(), human.getSurname() , human.getMiddlename(), human.getAge() ,year, group, cathedra, faculty);
+    }
+
+
+    private static Professor createProfessor(String cathedra, String faculty) throws IOException {
+        Human human = createHuman();
+        return new Professor(human.getFirstName(), human.getSurname() , human.getMiddlename(), human.getAge() , cathedra, faculty);
+    }
+
+    private static Cathedra createCathedra(String faculty) throws IOException {
+        String name = DataInput.getString("Input cathedra' name: ");
+        int amountOfStudents = DataInput.validateNumber("Input amount of students: ", 1, 100);
+        int amountOfProfessors = DataInput.validateNumber("Input amount of professors: ", 1, 100);
+        Student [] students = new Student[amountOfStudents];
+        Professor [] professors = new Professor[amountOfProfessors];
+        System.out.println("Set students: ");
+        for (int i = 0; i < amountOfStudents; i++) {
+            Student student = createStudent(name, faculty);
+            students[i] = student;
+            System.out.println("Student created");
+        }
+        System.out.println("Set professors: ");
+        for (int i = 0; i < amountOfProfessors; i++) {
+            Professor professor = createProfessor(name, faculty);
+            professors[i] = professor;
+            System.out.println("Professor created");
+        }
+        return new Cathedra(name, students, professors);
     }
 }
 
